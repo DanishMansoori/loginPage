afcf28396b14033ca45ca0621fefff9415bcbe13
Danish Mansoori <danishmansoori995@gmail.com>
2026-05-11 11:27:30 +0530

    updated sidebar

--- src/App.js
+++ src/App.js
@@ -1,200 +1,480 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
 
+  constructor(props) {
+    super(props);
 
-  constructor(props){
-    super(props)
     this.state = {
-      currentView : "Signup",
-      signup : {username : "", email : "" , password : ""},
-      login : {
-        usernameOrEmail : "" , password : ""
+
+      currentView: "Signup",
+
+      signup: {
+        username: "",
+        email: "",
+        password: ""
       },
 
-      
-      reset : {email : ""},
-      
-    }
+      login: {
+        usernameOrEmail: "",
+        password: ""
+      },
+
+      reset: {
+        email: ""
+      },
+
+      errors: {}
+
+    };
   }
 
-  chnageView = (view) =>{
-    this.setState ({
-      
-currentView : view,
 
-})
+
+  // Change Pages
+  chnageView = (view) => {
+
+    this.setState({
+      currentView: view,
+      errors: {}
+    });
+
   };
 
 
-  // typing k liye 
 
-  handleChange = (formName , field , value ) =>{
+
+  // Input Typing
+  handleChange = (formName, field, value) => {
+
     this.setState({
-      
-      [formName] : {...this.state[formName], [field] : value,}
 
-    })
-  }
-  
-  
-  
-  
+      [formName]: {
+        ...this.state[formName],
+        [field]: value,
+      }
 
-  
-  
+    });
 
-  // signup page 
-  
-  renderSignup(){
+  };
 
-    return(
-      
-      
+
+
+
+
+  // Signup Validation
+  validateSignup = () => {
+
+    let errors = {};
+
+    if (!this.state.signup.username) {
+      errors.username = "Username is required";
+    }
+
+    if (!this.state.signup.email) {
+      errors.email = "Email is required";
+    }
+
+    if (!this.state.signup.password) {
+      errors.password = "Password is required";
+    }
+
+    this.setState({ errors });
+
+    return Object.keys(errors).length === 0;
+  };
+
+
+
+
+
+
+  // Login Validation
+  validateLogin = () => {
+
+    let errors = {};
+
+    if (!this.state.login.usernameOrEmail) {
+      errors.usernameOrEmail =
+        "Username or Email is required";
+    }
+
+    if (!this.state.login.password) {
+      errors.loginPassword =
+        "Password is required";
+    }
+
+    this.setState({ errors });
+
+    return Object.keys(errors).length === 0;
+  };
+
+
+
+
+
+
+  // Reset Validation
+  validateReset = () => {
+
+    let errors = {};
+
+    if (!this.state.reset.email) {
+      errors.resetEmail = "Email is required";
+    }
+
+    this.setState({ errors });
+
+    return Object.keys(errors).length === 0;
+  };
+
+
+
+
+
+
+
+  // Signup Page
+  renderSignup() {
+
+    return (
+
       <div>
-      <h3>signup page </h3> 
 
-      <input type="text"
-            placeholder="Username"
-            value={this.state.signup.username}
-            onChange = {(e) =>  this.handleChange("signup", "username", e.target.value)} />
+        <h2>Signup Page</h2>
 
-
-            <input type="email"
-            placeholder="Email"
-            value={this.state.signup.email}
-            onChange={(e) =>this.handleChange("signup", "email", e.target.value)}
-            />
-
-           <input type="password"
-            placeholder="Password"
-            value={this.state.signup.password}
-            onChange={(e) =>this.handleChange("signup", "password", e.target.value)}
-            />
-
-
-
-        <button onClick={() =>alert("Signup Done")}>Sign up</button>
-
-
-
-
-
-
-
-
-
-      <button onClick={()=>{this.chnageView("Login")}}>Already Account ?</button>
-      
-      </div>
-    )
-  }
-  
-  
-  //login page
-  renderLogin (){
-    return(
-      <div>
-      
-        <h3>Login Page</h3>
-
-
-        <input type="text" 
-        
-        placeholder="Username/Email"
-        value ={this.state.login.usernameOrEmail
-        }
-        onChange={(e) =>this.handleChange("login","usernameOrEmail", e.target.value)}
-        
-        
-        
+        <input
+          type="text"
+          placeholder="Username"
+          value={this.state.signup.username}
+          onChange={(e) =>
+            this.handleChange(
+              "signup",
+              "username",
+              e.target.value
+            )
+          }
         />
 
- <input type="password" 
-        
-        placeholder="password"
-        value ={this.state.login.password
-        }
-        onChange={(e) =>this.handleChange("login","password", e.target.value)}
-        
-        
-        
+        <p className="error">
+          {this.state.errors.username}
+        </p>
+
+
+
+
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={this.state.signup.email}
+          onChange={(e) =>
+            this.handleChange(
+              "signup",
+              "email",
+              e.target.value
+            )
+          }
         />
 
-
-
-
-        <button onClick={()=> alert("Login Done")}>Login</button>
-   
-        <button onClick={()=>this.chnageView("Signup")}>Create Account</button>
-
-
-        <br />
-       <p onClick = {()=> this.chnageView("Reset")} > 
-        lost your password?
-           </p>
-  
-  </div>)
-  }
-  
-  
-  
-  
-  // reset function
-  
-  renderReset(){
-    return(
-      <>
-        <h3>Passsword Reset</h3>
-
-        <input type="email"
-        placeholder="Enter your email"
-        value={this.state.reset.email}
-        onChange={(e)=>this.handleChange("reset", "email", e.target.value)} />
-
-       <button onClick={()=> alert("Reset link sent")}>Send Reset Link</button>
-
-     <button onClick={()=>this.chnageView("Login")}>Back to Login</button>
-
-       
-       </>
+        <p className="error">
+          {this.state.errors.email}
+        </p>
 
 
 
 
-    )
-  }
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={this.state.signup.password}
+          onChange={(e) =>
+            this.handleChange(
+              "signup",
+              "password",
+              e.target.value
+            )
+          }
+        />
+
+        <p className="error">
+          {this.state.errors.password}
+        </p>
 
 
 
-  render(){
-    return(
 
 
-      <div className="page">
-        <div className="card">
+        <button onClick={() => {
 
-      {this.state.currentView === "Signup"  && this.renderSignup()}
-      {this.state.currentView === "Login"  && this.renderLogin()}
-      {this.state.currentView === "Reset"  && this.renderReset()}
-</div>
-      
-<br />
-<br />
-{/* <PasswordForm/> */}
-<br />
-<br />
-<br />
-      
-      
-      
+          if (this.validateSignup()) {
+
+            alert("Signup Done");
+
+          }
+
+        }}>
+          Signup
+        </button>
+
+
+
+
+
+        <button
+          onClick={() =>
+            this.chnageView("Login")
+          }
+        >
+          Already Account ?
+        </button>
+
       </div>
-    )
+    );
   }
 
 
 
 
+
+
+
+
+  // Login Page
+  renderLogin() {
+
+    return (
+
+      <div>
+
+        <h2>Login Page</h2>
+
+        <input
+          type="text"
+          placeholder="Username or Email"
+          value={this.state.login.usernameOrEmail}
+          onChange={(e) =>
+            this.handleChange(
+              "login",
+              "usernameOrEmail",
+              e.target.value
+            )
+          }
+        />
+
+        <p className="error">
+          {this.state.errors.usernameOrEmail}
+        </p>
+
+
+
+
+
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={this.state.login.password}
+          onChange={(e) =>
+            this.handleChange(
+              "login",
+              "password",
+              e.target.value
+            )
+          }
+        />
+
+        <p className="error">
+          {this.state.errors.loginPassword}
+        </p>
+
+
+
+
+
+
+        <button onClick={() => {
+
+          if (this.validateLogin()) {
+
+            alert("Login Done");
+
+          }
+
+        }}>
+          Login
+        </button>
+
+
+
+
+
+
+        <button
+          onClick={() =>
+            this.chnageView("Signup")
+          }
+        >
+          Create Account
+        </button>
+
+
+
+
+
+
+        <p
+          className="forgot"
+          onClick={() =>
+            this.chnageView("Reset")
+          }
+        >
+          Lost your password?
+        </p>
+
+      </div>
+    );
+  }
+
+
+
+
+
+
+
+
+  // Reset Page
+  renderReset() {
+
+    return (
+
+      <div>
+
+        <h2>Password Reset</h2>
+
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={this.state.reset.email}
+          onChange={(e) =>
+            this.handleChange(
+              "reset",
+              "email",
+              e.target.value
+            )
+          }
+        />
+
+        <p className="error">
+          {this.state.errors.resetEmail}
+        </p>
+
+
+
+
+
+
+        <button onClick={() => {
+
+          if (this.validateReset()) {
+
+            alert("Reset Link Sent");
+
+          }
+
+        }}>
+          Send Reset Link
+        </button>
+
+
+
+
+
+
+        <button
+          onClick={() =>
+            this.chnageView("Login")
+          }
+        >
+          Back To Login
+        </button>
+
+      </div>
+    );
+  }
+
+
+
+
+
+
+
+
+
+  render() {
+
+    return (
+
+      <div className="container">
+
+        {/* Sidebar */}
+        <div className="sidebar">
+
+          <h2>Auth Panel</h2>
+
+          <button
+            onClick={() =>
+              this.chnageView("Signup")
+            }
+          >
+            Signup
+          </button>
+
+          <button
+            onClick={() =>
+              this.chnageView("Login")
+            }
+          >
+            Login
+          </button>
+
+          <button
+            onClick={() =>
+              this.chnageView("Reset")
+            }
+          >
+            Reset
+          </button>
+
+        </div>
+
+
+
+
+
+        {/* Main Content */}
+        <div className="main-content">
+
+          <div className="card">
+
+            {this.state.currentView === "Signup" &&
+              this.renderSignup()}
+
+            {this.state.currentView === "Login" &&
+              this.renderLogin()}
+
+            {this.state.currentView === "Reset" &&
+              this.renderReset()}
+
+          </div>
+
+        </div>
+
+      </div>
+    );
+  }
 }
+
 export default App;
